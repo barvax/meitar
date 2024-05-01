03c0bf04cd7eb3a55cc3ab0e61e4b4559f69cbb3
barvax <69000340+barvax@users.noreply.github.com>
2024-05-01 20:03:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('burger-toggle').addEventListener('click', function() {
+    const menuContent = document.querySelector('.menu-content');
+    menuContent.classList.toggle('hidden');
+});
